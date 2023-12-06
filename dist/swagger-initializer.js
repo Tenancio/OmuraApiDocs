fc7e06bdd3816f5cd52e5b5838c4a887339578bf
Tenancio <contact@tenancio.com>
2023-12-06 14:08:18 +0000

    turn off deep linking?

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -5,7 +5,7 @@ window.onload = function() {
   window.ui = SwaggerUIBundle({
     url: "swagger.yaml",
     dom_id: '#swagger-ui',
-    deepLinking: true,
+    deepLinking: false,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
